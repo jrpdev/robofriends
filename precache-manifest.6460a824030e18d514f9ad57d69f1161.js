b9e5fc0a32b9e41c6fdee81cd51a0aa770567cb5
jrpdev <51519413+jrpdev@users.noreply.github.com>
2019-06-22 21:02:12 -0500

    Updates

---
+++ precache-manifest.6460a824030e18d514f9ad57d69f1161.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63e69c7b1b10d08969c7e1c18f375eb8",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "59522a08c6b1d3178e8a",
+    "url": "/robofriends/static/css/2.3cf8dc49.chunk.css"
+  },
+  {
+    "revision": "e0084da1794d54d16906",
+    "url": "/robofriends/static/css/main.786361ef.chunk.css"
+  },
+  {
+    "revision": "59522a08c6b1d3178e8a",
+    "url": "/robofriends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "e0084da1794d54d16906",
+    "url": "/robofriends/static/js/main.2a41b7f5.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
